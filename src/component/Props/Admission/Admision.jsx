3ee66e4814afd5b44e3015171d87bdf9901d40c7
MIR <pbank0001@gmail.com>
2023-12-05 11:20:48 +0100

    Update script.js(schoolproject)

--- src/component/Props/Admission/Admision.jsx
+++ src/component/Props/Admission/Admision.jsx
@@ -8,7 +8,7 @@ const Admision = () => {
             <Prop
                 icon={<ion-icon name="calendar-outline"></ion-icon>}
                 aim= 'ADMISSION'
-                content='The admission process at Tunmise is designed to be seamless and transparent. We welcome students from diverse backgrounds and offer equal opportunities for all. To apply, simply fill out our online application form, provide the required documents, and schedule an admission assessment.'
+                content='The admission process at Tunmise is designed to be seamless and transparent. We welcome students from diverse backgrounds and offer equal opportunities for all. To apply, simply fill out our online application form.'
              />
           </div>
     </div>
